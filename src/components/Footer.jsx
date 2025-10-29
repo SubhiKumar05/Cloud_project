1374ef6935be139ad18184d43e2b88438696ab85
Subhi Kumar <subhik60@gmail.com>
2025-10-29 20:58:32 +0530

    Fixed Footer href removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,33 +17,8 @@ const Footer = () => {
         © 2025 Berliner Diaries | All rights reserved
       </p>
       <p style={{ margin: "5px 0" }}>
-        Follow us on:&nbsp;
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "#B87333", margin: "0 5px" }}
-        >
-          Instagram
-        </a>
-        |
-        <a
-          href="https://github.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "#B87333", margin: "0 5px" }}
-        >
-          GitHub
-        </a>
-        |
-        <a
-          href="https://linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "#B87333", margin: "0 5px" }}
-        >
-          LinkedIn
-        </a>
+        
+       
       </p>
     </footer>
   );
